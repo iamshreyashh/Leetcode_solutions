6926aeab835a135c74b3c508bde8663acba6349d
Shreyash Tripathi <83861666+iamshreyashh@users.noreply.github.com>
2025-05-05 21:56:24 +0530

    Time: 4 ms (63.32%), Space: 18.9 MB (30.9%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,12 +1,11 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        arb_stack = []
-        zero_counter = 0
-        for i in nums:
-            if i == 0:
-                zero_counter+=1
-            else:
-                arb_stack.append(i)
-        nums[:] = arb_stack[:] + [0]*zero_counter
-       
-        
+        insert_pos = 0
+
+        for num in nums:
+            if num != 0:
+                nums[insert_pos] = num
+                insert_pos += 1
+
+        for i in range(insert_pos, len(nums)):
+            nums[i] = 0
